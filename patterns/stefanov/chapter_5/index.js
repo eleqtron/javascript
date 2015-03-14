64b746596f067f54b6f6f5aa2e1da740d7c06755
evgeniy <eleqtron@yandex.ru>
2015-03-15 01:21:12 +0300

    chp5

--- patterns/stefanov/chapter_5/index.js
+++ patterns/stefanov/chapter_5/index.js
@@ -4,5 +4,16 @@ function Gadget() {
     return name;
   }
 }
+
+Gadget.prototype = (function() {
+  var browser = 'Mobile Webkit';
+  return {
+    getBrowser: function() {
+      return browser
+    }
+  }
+}());
+
 var toy = new Gadget();
 console.log(toy.getName());
+console.log(toy.getBrowser());
