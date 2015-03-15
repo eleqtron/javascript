dbab72c7039fd9422572ef4d76001b3f38e5a670
evgeniy <eleqtron@yandex.ru>
2015-03-15 17:29:05 +0300

    chp6

--- patterns/stefanov/chapter_6/index.js
+++ patterns/stefanov/chapter_6/index.js
@@ -1,3 +1,7 @@
+function log(message) {
+  console.log(message);
+}
+
 (function() {
   function Parent(name) {
     this.name = name || 'Adam';
@@ -17,6 +21,34 @@
 
   var kid = new Child();
   kid.name = 'Patrick';
-  console.log(kid.say());
+//  log(kid.say());
 
+}());
+
+
+(function() {
+
+  var article, blog, page;
+
+  function Child(a,b,c,d) {
+    Parent.apply(this, arguments)
+  }
+
+  function Article() {
+    this.tags = ['js','css'];
+  }
+  article = new Article();
+
+  function BlogPost() {}
+  BlogPost.prototype = article
+  blog = new BlogPost();
+
+  function StaticPage() {
+    Article.call(this)
+  }
+  page = new StaticPage();
+
+//  log(article.hasOwnProperty('tags'));
+//  log(blog.hasOwnProperty('tags'));
+//  log(page.hasOwnProperty('tags'));
 }());
