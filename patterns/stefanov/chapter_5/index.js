c67aaf4db9fab42531519e05147375e661811965
evgeniy <eleqtron@yandex.ru>
2015-03-15 11:27:09 +0300

    chp5

--- patterns/stefanov/chapter_5/index.js
+++ patterns/stefanov/chapter_5/index.js
@@ -37,4 +37,71 @@ var newobj = (function() {
     }
   }
 }());
-console.log(myobj.getName());
+//console.log(myobj.getName());
+
+function Sandbox() {
+  var
+    args = Array.prototype.slice.call(arguments),
+    callback = args.pop(),
+    modules = (args[0] && typeof args[0] === 'string') ? args : args[0],
+    i;
+
+  if(!(this instanceof Sandbox)) {
+    return new Sandbox(modules,callback);
+  }
+
+  this.a = 1;
+  this.b = 2;
+
+  if(!modules || modules === '*') {
+    modules = [];
+    for(i in Sandbox.modules) {
+      if(Sandbox.modules.hasOwnProperty(i)) {
+        modules.push(i);
+      }
+    }
+  }
+
+  for(i=0; i<modules.length; i+=1) {
+    Sandbox.modules[modules[i]](this);
+  }
+
+  callback(this);
+
+}
+
+Sandbox.prototype = {
+  name: 'My App',
+  version: '1.0',
+  getName: function() {
+    return this.name;
+  },
+  getVersion: function() {
+    return this.version;
+  }
+
+};
+
+Sandbox.modules = {};
+
+Sandbox.modules.dom = function(box) {
+  box.getElement = function() { return 'Sandbox.modules.dom.getElement' };
+  box.getStyle = function() { return 'Sandbox.modules.dom.getStyle' };
+  box.foo = 'bar';
+};
+
+Sandbox.modules.event = function(box) {
+  box.attachEvent = function() { return 'Sandbox.modules.event.attachEvent' };
+  box.detachEvent = function() { return 'Sandbox.modules.event.detachEvent' };
+};
+
+Sandbox.modules.ajax = function(box) {
+  box.makeRequest = function() { return 'Sandbox.modules.ajax.makeRequest' };
+  box.getResponse = function() { return 'Sandbox.modules.ajax.getResponse' };
+};
+
+Sandbox('ajax', 'dom', function(box) {
+  console.log(box.getElement());
+  console.log(box.getVersion());
+  console.log('Sandbox work!');
+});
