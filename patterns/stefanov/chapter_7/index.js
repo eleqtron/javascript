ae86d1e32893ff067c6e2f41405599dbbce04e3b
evgeniy <eleqtron@yandex.ru>
2015-03-16 11:42:43 +0300

    chp7 singleton

--- patterns/stefanov/chapter_7/index.js
+++ patterns/stefanov/chapter_7/index.js
@@ -118,4 +118,58 @@ function singleton() {
 
 }
 
-singleton();
+//singleton();
+
+function fabric() {
+
+  function CarMaker() {}
+
+  CarMaker.prototype.drive = function() {
+    return 'Vroom, I have ' + this.doors + ' doors!';
+  };
+
+  CarMaker.factory = function(type) {
+    var
+      constr = type,
+      newcar;
+
+    if(typeof CarMaker[constr] !== 'function') {
+      throw {
+        name: 'Error',
+        message: constr + 'doesn\'t exist'
+      }
+    }
+
+    if (typeof CarMaker[constr].prototype.drive !== 'function') {
+      CarMaker[constr].prototype = new CarMaker();
+    }
+
+    newcar = new CarMaker[constr]();
+    return newcar;
+
+  };
+
+  CarMaker.Compact = function() {
+    this.doors = 4;
+  };
+
+  CarMaker.Convertible = function() {
+    this.doors = 2;
+  };
+
+  CarMaker.SUV = function() {
+    this.doors = 24;
+  };
+
+  var
+    corolla = CarMaker.factory('Compact'),
+    solstice = CarMaker.factory('Convertible'),
+    cherokee = CarMaker.factory('SUV');
+
+  console.log(corolla.drive());
+  console.log(solstice.drive());
+  console.log(cherokee.drive());
+
+}
+
+fabric();
