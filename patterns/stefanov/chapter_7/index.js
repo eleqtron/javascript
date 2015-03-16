d3abde90d1a71c89d368d0188b5a5b3e72eb283a
evgeniy <eleqtron@yandex.ru>
2015-03-16 11:08:16 +0300

    chp7 singleton

--- patterns/stefanov/chapter_7/index.js
+++ patterns/stefanov/chapter_7/index.js
@@ -3,19 +3,48 @@
   //variant 1
   //экземпляр в статическом свойстве
   // - свойство instance является общедоступным
-  function Universe() {
-    if(typeof Universe.instance === 'object') {
-      return Universe.instance
-    }
-    this.start_time = 0;
-    this.bang = 'Big'
-    Universe.instance = this;
-    return this;
-  }
+  (function() {
 
-  var uni1,uni2;
-  uni1 = new Universe();
-  uni2 = new Universe();
-  console.log(uni1 === uni2)
+    function Universe() {
+      if(typeof Universe.instance === 'object') {
+        return Universe.instance
+      }
+      this.start_time = 0;
+      this.bang = 'Big'
+      Universe.instance = this;
+      return this;
+    }
+
+    var uni1,uni2;
+    uni1 = new Universe();
+    uni2 = new Universe();
+    console.log(uni1 === uni2)
+
+  }());
+
+  //variant 2
+  //экземпляр в замыкании
+  // - все что будет добавлено в прототип Universe будет недоступно экземпляру, созданному оригинальной реализацией
+  (function() {
+
+    function Universe() {
+
+      var instance = this;
+
+      this.start_time = 0;
+      this.bang = 'Big';
+
+      Universe = function() {
+        return instance
+      };
+
+    }
+
+    var uni1,uni2;
+    uni1 = new Universe();
+    uni2 = new Universe();
+    console.log(uni1 === uni2)
+
+  }());
 
 }());
