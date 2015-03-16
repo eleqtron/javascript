b63a43785f6d1d659ace80f48a21210dd0c66bae
evgeniy <eleqtron@yandex.ru>
2015-03-16 11:17:36 +0300

    chp7 singleton

--- patterns/stefanov/chapter_7/index.js
+++ patterns/stefanov/chapter_7/index.js
@@ -1,4 +1,4 @@
-(function singleton() {
+function singleton() {
 
   //variant 1
   //экземпляр в статическом свойстве
@@ -18,7 +18,7 @@
     var uni1,uni2;
     uni1 = new Universe();
     uni2 = new Universe();
-    console.log(uni1 === uni2)
+//    console.log(uni1 === uni2)
 
   }());
 
@@ -41,10 +41,50 @@
     }
 
     var uni1,uni2;
+    Universe.prototype.nothing = true;
     uni1 = new Universe();
+    Universe.prototype.everything = true;
     uni2 = new Universe();
-    console.log(uni1 === uni2)
+    console.log('uni1 === uni2: ', uni1 === uni2);
+    console.log('has nothing && everything: ', uni1.nothing && uni1.everything && uni2.nothing && uni2.everything);
+    console.log('bang: ', uni1.bang);
 
   }());
 
-}());
+  //variant 3
+  //экземпляр в замыкании с сохранением свойств добавленных в прототип
+  (function() {
+
+    function Universe() {
+
+      var instance = this;
+
+      Universe = function() {
+        return instance
+      };
+
+      Universe.prototype = this;
+      instance = new Universe();
+      instance.constructor = Universe;
+
+      instance.start_time = 0;
+      instance.bang = 'Big';
+
+      return instance;
+    }
+
+    var uni1,uni2;
+    Universe.prototype.nothing = true;
+    uni1 = new Universe();
+    Universe.prototype.everything = true;
+    uni2 = new Universe();
+    console.log('uni1 === uni2: ', uni1 === uni2);
+    console.log('has nothing && everything: ', uni1.nothing && uni1.everything && uni2.nothing && uni2.everything);
+    console.log('bang: ', uni1.bang);
+    console.log('uni1.constructor == Universe: ', uni1.constructor == Universe);
+
+  }());
+
+}
+
+singleton();
