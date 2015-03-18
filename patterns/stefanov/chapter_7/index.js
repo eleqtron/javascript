05811c344db61ba7bc7255ee9d0de0252c83ae70
evgeniy <eleqtron@yandex.ru>
2015-03-18 10:30:44 +0300

    chp7 facade

--- patterns/stefanov/chapter_7/index.js
+++ patterns/stefanov/chapter_7/index.js
@@ -449,6 +449,19 @@ function facade() {
       e.preventDefault();
       e.stopPropagation();
     }
+  };
+
+  // a simple facade that masks the various browser-specific methods
+  function addEvent( element, event, callback ) {
+
+    if( window.addEventListener ) {
+      element.addEventListener( event, callback, false );
+    } else if( document.attachEvent ) {
+      element.attachEvent( 'on' + event, callback );
+    } else {
+      element[ 'on' + event ] = callback;
+    }
+
   }
 }
 
