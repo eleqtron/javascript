7493388c3b4e1f1404f8e14c488bcfb5ae1a733c
evgeniy <eleqtron@yandex.ru>
2015-03-23 18:07:10 +0300

    js ninja chapter3

--- ninja/app.js
+++ ninja/app.js
@@ -11,50 +11,7 @@ function log() {
     }
   }
 }
-
-(function() {
-  var queue = [], paused = false, results;
-
-  this.test = function test(name, fn) {
-    queue.push(function(){
-      results = document.getElementById('results');
-      results = assert(true, name).appendChild( document.createElement('ul') );
-      fn();
-    });
-    runTest();
-  };
-
-  this.pause = function() {
-    paused = true;
-  };
-
-  this.resume = function() {
-    paused = false;
-    setTimeout(runTest, 1);
-  };
-
-  this.assert = function(value, desc) {
-    var li = document.createElement('li');
-    li.className = value ? 'pass' : 'fail';
-    li.appendChild(document.createTextNode(desc));
-    results.appendChild(li);
-    if(!value) {
-      li.parentNode.parentNode.className = 'fail';
-    }
-    return li;
-  };
-
-  function runTest() {
-    if(!paused && queue.length) {
-      queue.shift()();
-      if(!paused) {
-        resume();
-      }
-    }
-  }
-
-}());
-
+/*
 window.onload = function() {
   test("Async Test #1", function() {
     pause();
@@ -71,5 +28,5 @@ window.onload = function() {
     }, 1000);
   });
 };
-
+*/
 
