f24c55e2da15c0c0bcc9b59b69443fb492b8b6b5
evgeniy <eleqtron@yandex.ru>
2015-03-30 00:25:07 +0300

    js ninja cp3

--- ninja/chapter_3/index.js
+++ ninja/chapter_3/index.js
@@ -55,5 +55,24 @@ window.onload = function() {
     assert( ninja2.result === 10, 'juggled via call' );
   });
 
+  test('smallest & largest', function() {
+    var arr = [1,2,3,4,5,6,7];
+    assert( smallest(arr) === 1, 'Smallest work!' );
+    assert( largest(arr)  === 7, 'Largest work!' );
+  });
 
 };
+
+function isPalindrome(text) {
+  if(text.length <= 1) return true;
+  if(text.charAt(0) != text.charAt(text.length - 1)) return false;
+  return isPalindrome(text.substr(1, text.length -2 ));
+}
+
+function smallest(array) {
+  return Math.min.apply(Math, array);
+}
+
+function largest(array) {
+  return Math.max.apply(Math, array);
+}
